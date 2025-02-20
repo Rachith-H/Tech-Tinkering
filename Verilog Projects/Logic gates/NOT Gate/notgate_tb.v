23993953c086673cef2183f0f8afdbe327ce741b
Rachith-H <157898134+Rachith-H@users.noreply.github.com>
2025-02-20 22:40:37 +0530

    Update notgate_tb.v

--- Verilog Projects/Logic gates/NOT Gate/notgate_tb.v
+++ Verilog Projects/Logic gates/NOT Gate/notgate_tb.v
@@ -1,30 +1,11 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.11.2024 17:57:00
-// Design Name: 
-// Module Name: notgate_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module notgate_tb;
-reg A;
-wire Z;
-notgate uut (.A(A),.Z(Z));
-initial begin
-A=0 ; #100;
-A=1 ; #100;
-$finish;
+reg a;
+wire y;
+notgate uut (.a(a),.y(y));
+initial begin 
+a = 1; #100;
+a = 0; #100;
+a = 1; #100;
+a = 0; #100;
 end
 endmodule
