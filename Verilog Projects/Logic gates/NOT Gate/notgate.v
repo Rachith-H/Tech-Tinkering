3c4cd1f1c1676140addd48b3c7ed3b4a93855c4e
Rachith-H <157898134+Rachith-H@users.noreply.github.com>
2025-02-20 22:40:01 +0530

    Update notgate.v

--- Verilog Projects/Logic gates/NOT Gate/notgate.v
+++ Verilog Projects/Logic gates/NOT Gate/notgate.v
@@ -1,29 +1,7 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.11.2024 17:56:07
-// Design Name: 
-// Module Name: notgate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-module notgate(
-input A, output Z
-    );
-    supply1 Vol;
-    supply0 GND;
-    pmos P1(Z,Vol,A);
-    nmos N1(Z,GND,A);
+module notgate(a,y);
+input a;
+output y;
+assign y = ~a;
 endmodule
+
+
